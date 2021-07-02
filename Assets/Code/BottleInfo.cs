2f85e2d69a0ead36f9658d9e063c2d737eac7528
ArielShabi <4ariel6@gmail.com>
2021-07-02 18:38:23 +0300

    Added bottle info

---
+++ Assets/Code/BottleInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleInfo
+{
+    public string Name { get; set; }
+    public DrinkCategory DrinkCategory {get;set;}
+    public Sprite Sprite { get; set; }
+}
