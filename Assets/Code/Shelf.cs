68a8b1ff124023aee602d6773863b9dac85b1d8c
ArielShabi <4ariel6@gmail.com>
2021-07-02 18:30:35 +0300

    added shelf

---
+++ Assets/Code/Shelf.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shelf : MonoBehaviour
+{
+    public GameObject BottlePrefab;
+    private GameObject[] _bottles;   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void FillShelf()
+    {
+
+    }
+}
