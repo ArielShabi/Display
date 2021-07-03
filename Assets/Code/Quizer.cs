91d0c52407eef3222eba69c33a8d7db86f8a1e3c
ArielShabi <4ariel6@gmail.com>
2021-07-03 09:35:01 +0300

    update quizer and manually import bottles

--- Assets/Code/Quizer.cs
+++ Assets/Code/Quizer.cs
@@ -3,13 +3,6 @@ using UnityEditor;
 using UnityEngine;
 
 public class Quizer : MonoBehaviour
-{
-    public string BootlesInfoPath;
-
-    public BottleInfo[] _bottles;
-
-    void Start()
-    {
-        var allAssets = AssetDatabase.LoadAllAssetsAtPath(BootlesInfoPath);
-        _bottles = Array.ConvertAll<object, BottleInfo>(allAssets, obj => (BottleInfo)obj);
-    }
+{    
+    public BottleInfo[] _bottles;    
+}
