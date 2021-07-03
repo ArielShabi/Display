7d7d6ad4e28a3d6db62b09bfea85994ad088b557
ArielShabi <4ariel6@gmail.com>
2021-07-04 00:00:39 +0300

    update method name

--- Assets/Code/AnswerUi.cs
+++ Assets/Code/AnswerUi.cs
@@ -5,13 +5,12 @@ using UnityEngine.UI;
 public class AnswerUi : MonoBehaviour
 {
     private Image imagedAnswer;
-    public Sprite Test;
     void Start()
     {
-        imagedAnswer = GetComponentsInChildren<Image>().First(image => image.gameObject != gameObject);    
+        imagedAnswer = GetComponentsInChildren<Image>().First(image => image.gameObject != gameObject);
     }
 
-    public void UpdateAnswerImage(Sprite newImage)
+    public void SetAnswerImage(Sprite newImage)
     {
         imagedAnswer.sprite = newImage;
     }
